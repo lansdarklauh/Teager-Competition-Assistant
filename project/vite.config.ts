155a23ec3be1fe276b5e45c0bf96f426e2151d09
lansdarklauh <68766437+lansdarklauh@users.noreply.github.com>
2023-08-28 22:36:27 +0800

    修改合并

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-// import legacy from '@vitejs/plugin-legacy';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './',
+  base: './',
   publicDir: 'public',
   plugins: [
-    // legacy({
-    //   targets: ['defaults', 'not IE 11']
-    // }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
     react()
   ],
   resolve: {
