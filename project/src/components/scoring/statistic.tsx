e5ac4483e7ff60ff0e6ee2cab649f30ce21efdb6
lansdarklauh <68766437+lansdarklauh@users.noreply.github.com>
2023-08-29 23:31:47 +0800

    添加electron

--- project/src/components/scoring/statistic.tsx
+++ project/src/components/scoring/statistic.tsx
@@ -106,7 +106,7 @@ const Statistic = forwardRef((_props, ref) => {
 
     // 显示排名窗口方便捕获
     const showRankHandle = () => {
-        window.open(location.pathname + '/#/showRank', "_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=800, height=400")
+        window.open(location.pathname + '#/showRank', "_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=800, height=400")
     }
 
 
